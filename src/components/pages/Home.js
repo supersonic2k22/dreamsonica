6d5ad557c1a60d7f460e7cc535facabcac937daa
waiter-developer <yatsunenko90@gmail.com>
2020-12-16 11:50:17 +0200

    Added gifLoader.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,14 +9,11 @@ import GlobalFuture from "../GlobalFuture_DS";
 import Form from "../Form_DS";
 import Footer from "../Footer_DS";
 import { Sugar } from 'react-preloaders';
+import  gifLoader  from '../../static/video/right-left.gif.gif';
 import {
     Events,
     scroller,
 } from 'react-scroll'
-
-
-
-
 
 export default class Home extends Component {
 
@@ -47,21 +44,24 @@ export default class Home extends Component {
         })
     }
 
-
-
     fakeRequest = () => {
-        return new Promise(resolve => setTimeout(()=>resolve(), 3500))
+        return new Promise(resolve => setTimeout(()=>resolve(), 2000))
     }
 
     componentWillUnmount() {
         Events.scrollEvent.remove('begin');
         Events.scrollEvent.remove('end');
+        this.fakeRequest().then(null)
     }
 
     render() {
 
         if(this.state.loading) {
-            return <Sugar color={'orangered'} time={3000}/>
+            return (
+                <div style={{position:"fixed", top: '0', bottom: '0', left: '0', right: '0', width: '100%', height:'100%'}}>
+                    <img src={gifLoader} alt='Loader' style={{width: '100%', height: 'auto'}}/>
+                </div>
+            )
         }
 
         return (
